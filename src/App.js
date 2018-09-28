6450951e0a1c15d14cba7356196e54fa9ec91d7b
Lee Wignall <lee.j.wignall@gmail.com>
2018-09-28 14:15:45 +0100

    Test hex tesselation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+const HexTile = () => {
+  return (
+    <div>
+    <svg><polygon className="hex" points="30,13 22.5,26 7.5,26 0,13 7.5,0 22.5,0"></polygon></svg>
+    </div>
+  );
+}
+
+const GridColumn = () => {
+  return (
+    <div id="GridColumn">
+      <HexTile />
+      <HexTile />
+      <HexTile />
+    </div>
+  )
+}
+
+const Grid = () => {
+  return (
+  <div>
+    <GridColumn />
+    <GridColumn />
+    <GridColumn />
+  </div>
+)
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Grid />
       </div>
     );
   }
